45ec5802bc50a257cbed0ef37f6911af83b0f0a5
nOOb3167 <nOOb3167@gmail.com>
2023-10-24 14:27:14 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 // https://blog.xojo.com/2019/07/02/accessing-windows-runtime-winrt/
 // https://bytes.com/topic/c/answers/942922-virtual-pointer-accessing-vtable-class-inside-program
 
+#define _SILENCE_CXX17_CODECVT_HEADER_DEPRECATION_WARNING
+
+#include <locale>
+#include <codecvt>
+#include <format>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 #include <stdint.h>
 
-#include <Windows.h>
+#include <windows.h>
 #include <wrl.h>
 
 namespace wrl = Microsoft::WRL;
@@ -16,6 +22,8 @@ using namespace std;
 
 typedef void (*zfnc)(void);
 typedef HRESULT (*zgcs)(IUnknown* thiz, HSTRING* ret);
+
+#define WSTR2STR(x) (std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(x))
 
 zgcs zGetCalendarSystem;
 
@@ -31,35 +39,26 @@ UUID mkuuid(std::string s)
 void stuff()
 {
 	wrlw::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
-	wrlw::HString className;
-	if (FAILED(className.Set(L"Windows.Globalization.Calendar")))
-		throw std::runtime_error("");
+	wrlw::HStringReference className(L"Windows.Globalization.Calendar");
 
 	wrl::ComPtr<IInspectable> ii0;
-	if (FAILED(RoActivateInstance(className, &ii0)))
+	if (FAILED(RoActivateInstance(className.Get(), &ii0)))
 		throw std::runtime_error("");
 
 	wrl::ComPtr<IUnknown> iu0;
-	if (FAILED(ii0.As(&iu0)))
+	if (FAILED(ii0.AsIID(mkuuid("CA30221D-86D9-40FB-A26B-D44EB7CF08EA"), &iu0)))
 		throw std::runtime_error("");
 
-	wrl::ComPtr<IUnknown> iu1;
-	if (FAILED(iu0.AsIID(mkuuid("CA30221D-86D9-40FB-A26B-D44EB7CF08EA"), &iu1)))
-		throw std::runtime_error("");
-
-	IUnknown* zzz = iu1.Get();
-
-	zgcs** obj = (zgcs**)iu1.Get();
+	zgcs** obj = (zgcs**)iu0.Get();
 	zgcs*  vta = *obj;
 	zgcs*  vta12 = vta + 12;
 	zgcs   gcs = *vta12;
 
-	//uint64_t *vtab = (uint64_t*)iu1.Get();
-	//vtab = (uint64_t*)*vtab;
-	//zgcs gcs = *(zgcs*)(vtab + 12);
 	HSTRING rr;
-	if (FAILED(gcs(iu1.Get(), &rr)))
+	if (FAILED(gcs(iu0.Get(), &rr)))
 		throw std::runtime_error("");
+
+	println(cout, "{}", WSTR2STR(WindowsGetStringRawBuffer(rr, NULL)));
 }
 
 
