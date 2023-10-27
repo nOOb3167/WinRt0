3797aa0f10d823540633412c7621b719a8e0374e
nOOb3167 <nOOb3167@gmail.com>
2023-10-27 03:12:33 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -36,6 +36,7 @@
 #include <windows.devices.bluetooth.genericattributeprofile.h>
 
 #define WSTR2STR(x) (std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(x))
+#define CHK(x) { if (FAILED(x)) throw std::runtime_error("Failure[" #x "]"); }
 
 UUID mkuuid(std::string s);
 void d_hstring(HSTRING p);
@@ -66,7 +67,7 @@ typedef ABI::Windows::Foundation::ITypedEventHandler<ADV::BluetoothLEAdvertiseme
 typedef HRESULT (*zfScanningMode_Set)(IUnknown* thiz, int32_t bluetoothLEScanningMode);
 typedef HRESULT (*zfStart)(IUnknown* thiz);
 typedef HRESULT (*zfStop)(IUnknown* thiz);
-typedef HRESULT (*zfReceived)(IUnknown *thiz, zfReceivedTEH* handler, EventRegistrationToken *tok);
+typedef HRESULT (*zfReceived)(IUnknown *thiz, IUnknown* handler, EventRegistrationToken *tok);
 typedef HRESULT (*zfBluetoothAddress)(IUnknown* thiz, uint64_t *value);
 typedef HRESULT (*zfGetGattServicesWithCacheModeAsync)(IUnknown* thiz, int32_t bluetoothCacheMode, IUnknown** out);
 typedef HRESULT (*zfPut_Completed)(IUnknown* thiz, IUnknown* handler);
@@ -182,6 +183,11 @@ struct zIAsyncOperation
 	vt* vt;
 };
 
+
+template<typename T, typename U>
+decltype(T::vt) GetVt(wrl::ComPtr<U> u) { return ((T*)u.Get())->vt; }
+
+
 void d_hstring(HSTRING p)
 {
 	WindowsDeleteString(p);
@@ -230,28 +236,15 @@ public:
 	ULONG   STDMETHODCALLTYPE AddRef(void) override { InterlockedIncrement(&m_ref); return this->m_ref; }
 	ULONG   STDMETHODCALLTYPE Release(void) override { InterlockedDecrement(&m_ref); if (m_ref > 0) return m_ref; delete this; return 0; }
 	virtual HRESULT STDMETHODCALLTYPE Invoke(IInspectable *sender, IInspectable *args) {
-		wrl::ComPtr<IUnknown> watcher_iu;
-		if (FAILED(sender->QueryInterface(uuidIBluetoothLEAdvertisementWatcher, &watcher_iu)))
-			throw std::runtime_error("");
+		wrl::ComPtr<IUnknown> watcher;
+		wrl::ComPtr<IUnknown> args1;
+		wrl::ComPtr<IUnknown> args2;
 
-		wrl::ComPtr<IUnknown> args1_iu;
-		if (FAILED(args->QueryInterface(uuidIBluetoothLEAdvertisementReceivedEventArgs, &args1_iu)))
-			throw std::runtime_error("");
-		wrl::ComPtr<IUnknown> args2_iu;
-		if (FAILED(args->QueryInterface(uuidIBluetoothLEAdvertisementReceivedEventArgs2, &args2_iu)))
-			throw std::runtime_error("");
+		CHK(sender->QueryInterface(uuidIBluetoothLEAdvertisementWatcher, &watcher));
+		CHK(args->QueryInterface(uuidIBluetoothLEAdvertisementReceivedEventArgs, &args1));
+		CHK(args->QueryInterface(uuidIBluetoothLEAdvertisementReceivedEventArgs2, &args2));
 
-		uint64_t addr = 0;
-		if (FAILED(((zIBluetoothLEAdvertisementReceivedEventArgs*)args1_iu.Get())->vt->BluetoothAddress(args1_iu.Get(), &addr)))
-			throw std::runtime_error("");
-		m_addr = addr;
-
-		HSTRING rcn;
-		args->GetRuntimeClassName(&rcn);
-		ULONG ic;
-		IID* ia;
-		args->GetIids(&ic, &ia);
-		println(cout, "Invoke");
+		CHK(GetVt<zIBluetoothLEAdvertisementReceivedEventArgs>(args1)->BluetoothAddress(args1.Get(), &m_addr));
 
 		m_completed.signal();
 
@@ -274,16 +267,9 @@ public:
 	ULONG   STDMETHODCALLTYPE AddRef(void) override { InterlockedIncrement(&m_ref); return this->m_ref; }
 	ULONG   STDMETHODCALLTYPE Release(void) override { InterlockedDecrement(&m_ref); if (m_ref > 0) return m_ref; delete this; return 0; }
 	virtual HRESULT STDMETHODCALLTYPE Invoke(IInspectable *sender) {
-		wrl::ComPtr<IUnknown> _iu;
-		if (FAILED(sender->QueryInterface(uuidIAsyncOperation__BluetoothLEDevice__, &_iu)))
-			throw std::runtime_error("");
+		wrl::ComPtr<IUnknown> ledev;
 
-		HSTRING rcn;
-		sender->GetRuntimeClassName(&rcn);
-		ULONG ic;
-		IID* ia;
-		sender->GetIids(&ic, &ia);
-		println(cout, "Invoke");
+		CHK(sender->QueryInterface(uuidIAsyncOperation__BluetoothLEDevice__, &ledev));
 
 		m_completed.signal();
 
@@ -304,16 +290,9 @@ public:
 	ULONG   STDMETHODCALLTYPE AddRef(void) override { InterlockedIncrement(&m_ref); return this->m_ref; }
 	ULONG   STDMETHODCALLTYPE Release(void) override { InterlockedDecrement(&m_ref); if (m_ref > 0) return m_ref; delete this; return 0; }
 	virtual HRESULT STDMETHODCALLTYPE Invoke(IInspectable* sender) {
-		wrl::ComPtr<IUnknown> _iu;
-		if (FAILED(sender->QueryInterface(uuidIAsyncOperation__GattDeviceServicesResult_star__, &_iu)))
-			throw std::runtime_error("");
-
-		HSTRING rcn;
-		sender->GetRuntimeClassName(&rcn);
-		ULONG ic;
-		IID* ia;
-		sender->GetIids(&ic, &ia);
-		println(cout, "Invoke");
+		wrl::ComPtr<IUnknown> op;
+		
+		CHK(sender->QueryInterface(uuidIAsyncOperation__GattDeviceServicesResult_star__, &op));
 
 		m_completed.signal();
 
@@ -325,80 +304,51 @@ public:
 void stuff()
 {
 	wrlw::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
-	wrlw::HStringReference className(L"Windows.Globalization.Calendar");
 
-	wrl::ComPtr<IInspectable> ii0;
-	if (FAILED(RoActivateInstance(className.Get(), &ii0)))
-		throw std::runtime_error("");
+	wrl::ComPtr<IInspectable> watcher;
 
-	wrl::ComPtr<IUnknown> iu0;
-	if (FAILED(ii0.AsIID(mkuuid("CA30221D-86D9-40FB-A26B-D44EB7CF08EA"), &iu0)))
-		throw std::runtime_error("");
+	CHK(FAILED(RoActivateInstance(wrlw::HStringReference(L"Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher").Get(), &watcher)));
 
-	auto rr = make_unique<HSTRING>();
-	if (FAILED(((zICalendar*)iu0.Get())->vt->GetCalendarSystem(iu0.Get(), rr.get())))
-		throw std::runtime_error("");
+	GetVt<zIBluetoothLEAdvertisementWatcher>(watcher)->ScanningMode_Set(watcher.Get(), (int32_t)zBluetoothLEScanningMode::Active);
 
-	println(cout, "{}", WSTR2STR(WindowsGetStringRawBuffer(*rr, NULL)));
-
-	wrlw::HStringReference watcher_className(L"Windows.Devices.Bluetooth.Advertisement.BluetoothLEAdvertisementWatcher");
-
-	wrl::ComPtr<IInspectable> watcher_ii;
-	if (FAILED(RoActivateInstance(watcher_className.Get(), &watcher_ii)))
-		throw std::runtime_error("");
-
-	wrl::ComPtr<IUnknown> watcher_iu;
-	if (FAILED(watcher_ii.AsIID(uuidIBluetoothLEAdvertisementWatcher, &watcher_iu)))
-		throw std::runtime_error("");
-
-	((zIBluetoothLEAdvertisementWatcher*)watcher_iu.Get())->vt->ScanningMode_Set(watcher_iu.Get(), (int32_t)zBluetoothLEScanningMode::Active);
-
-	Cb* cb = new Cb();
-	Cb2* cb2 = new Cb2();
-	Cb3* cb3 = new Cb3();
+	wrl::ComPtr<Cb> cb = new Cb();
+	wrl::ComPtr<Cb2> cb2 = new Cb2();
+	wrl::ComPtr<Cb3> cb3 = new Cb3();
+	
 	EventRegistrationToken tok;
 
-	if (FAILED(((zIBluetoothLEAdvertisementWatcher*)watcher_iu.Get())->vt->Received(watcher_iu.Get(), (zfReceivedTEH *)cb, &tok)))
-		throw std::runtime_error("");
+	CHK(GetVt<zIBluetoothLEAdvertisementWatcher>(watcher)->Received(watcher.Get(), cb.Get(), &tok));
 
-	if (FAILED(((zIBluetoothLEAdvertisementWatcher*)watcher_iu.Get())->vt->Start(watcher_iu.Get())))
-		throw std::runtime_error("");
+	CHK(GetVt<zIBluetoothLEAdvertisementWatcher>(watcher)->Start(watcher.Get()));
 
 	cb->m_completed.wait();
 
-	if (FAILED(((zIBluetoothLEAdvertisementWatcher*)watcher_iu.Get())->vt->Stop(watcher_iu.Get())))
-		throw std::runtime_error("");
+	CHK(GetVt<zIBluetoothLEAdvertisementWatcher>(watcher)->Stop(watcher.Get()));
 
-	//RoGetActivationFactory(wrlw::HStringReference(L"Windows.Devices.Bluetooth.BluetoothLEDevice"), IID___x_ABI_CWindows_CDevices_CBluetooth_CIBluetoothLEDeviceStatics)
 	wrl::ComPtr<ABI::Windows::Devices::Bluetooth::IBluetoothLEDeviceStatics> ledevicesta;
-	if (FAILED(ABI::Windows::Foundation::GetActivationFactory(wrlw::HString::MakeReference(L"Windows.Devices.Bluetooth.BluetoothLEDevice").Get(), &ledevicesta)))
-		throw std::runtime_error("");
+	CHK(ABI::Windows::Foundation::GetActivationFactory(wrlw::HString::MakeReference(L"Windows.Devices.Bluetooth.BluetoothLEDevice").Get(), &ledevicesta))
 
 	wrl::ComPtr<ABI::Windows::Foundation::IAsyncOperation<ABI::Windows::Devices::Bluetooth::BluetoothLEDevice*>> fbaa_op;
-	if (FAILED(ledevicesta->FromBluetoothAddressAsync(cb->m_addr, &fbaa_op)))
-		throw std::runtime_error("");
+	CHK(ledevicesta->FromBluetoothAddressAsync(cb->m_addr, &fbaa_op));
 
-	if (FAILED(fbaa_op->put_Completed((ABI::Windows::Foundation::IAsyncOperationCompletedHandler<ABI::Windows::Devices::Bluetooth::BluetoothLEDevice*>*)cb2)))
-		throw std::runtime_error("");
+	CHK(fbaa_op->put_Completed((ABI::Windows::Foundation::IAsyncOperationCompletedHandler<ABI::Windows::Devices::Bluetooth::BluetoothLEDevice*>*)cb2.Get()));
+
 	cb2->m_completed.wait();
+
 	wrl::ComPtr<ABI::Windows::Devices::Bluetooth::IBluetoothLEDevice> ledev;
 	wrl::ComPtr<IUnknown> ledev3;
-	if (FAILED(fbaa_op->GetResults(&ledev)))
-		throw std::runtime_error("");
+	CHK(fbaa_op->GetResults(&ledev));
 
-	if (FAILED(ledev.AsIID(uuidIBluetoothLEDevice3, &ledev3)))
-		throw std::runtime_error("");
+	CHK(ledev.AsIID(uuidIBluetoothLEDevice3, &ledev3));
 
 	wrl::ComPtr<IUnknown> ggswcma_op;
-	if (FAILED(((zIBluetoothLEDevice3*)ledev3.Get())->vt->GetGattServicesWithCacheModeAsync(ledev3.Get(), (int32_t)zBluetoothCacheMode::Uncached, &ggswcma_op)))
-		throw std::runtime_error("");
+	CHK(GetVt<zIBluetoothLEDevice3>(ledev3)->GetGattServicesWithCacheModeAsync(ledev3.Get(), (int32_t)zBluetoothCacheMode::Uncached, &ggswcma_op));
 
 	wrl::ComPtr<IUnknown> qqq;
-	if (FAILED(ggswcma_op->QueryInterface(uuidIAsyncOperation__GattDeviceServicesResult_star__, &qqq)))
-		throw std::runtime_error("");
+	CHK(ggswcma_op->QueryInterface(uuidIAsyncOperation__GattDeviceServicesResult_star__, &qqq));
 
-	if (FAILED(((zIAsyncOperation*)qqq.Get())->vt->Put_Completed(qqq.Get(), cb3)))
-		throw std::runtime_error("");
+	CHK(GetVt<zIAsyncOperation>(qqq)->Put_Completed(qqq.Get(), cb3.Get()));
+
 	cb3->m_completed.wait();
 
 	println(cout, "");
