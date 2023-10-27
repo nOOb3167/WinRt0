261beba3f9fbf62dfec39c6708a084a45487ed0c
nOOb3167 <nOOb3167@gmail.com>
2023-10-27 20:39:46 +0200

    stuff

--- main.cpp
+++ main.cpp
@@ -43,16 +43,39 @@ using namespace bt;
 using namespace std;
 
 
-void stuff()
+void deviceFromBluetoothAddress(uint64_t bluetoothAddress, wrl::ComPtr<IUnknown> *outBluetoothLEDevice, wrl::ComPtr<IUnknown>* outBluetoothLEDevice3)
 {
-	wrlw::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+	wrl::ComPtr<IUnknown> bluetoothLEDeviceStatics;
+	wrl::ComPtr<IUnknown> bluetoothLEDevice;
+	wrl::ComPtr<IUnknown> bluetoothLEDevice3;
 
-	auto scanner = make_unique<Scanner>();
+	wrl::ComPtr<IUnknown> asyncOperation;
 
-	scanner->wait();
+	MCompleted completed;
+	wrl::ComPtr<ComHandler_IAsyncOperationCompletedHandler__BluetoothLEDevice__> cb2 = new ComHandler_IAsyncOperationCompletedHandler__BluetoothLEDevice__(
+		[&completed]() {
+			completed.signal();
+		}
+	);
 
-	wrl::ComPtr<IUnknown> ledevicesta;
-	CHK(RoGetActivationFactory(wrlw::HString::MakeReference(L"Windows.Devices.Bluetooth.BluetoothLEDevice").Get(), uuidIBluetoothLEDeviceStatics, &ledevicesta));
+	CHK(RoGetActivationFactory(wrlw::HString::MakeReference(L"Windows.Devices.Bluetooth.BluetoothLEDevice").Get(), uuidIBluetoothLEDeviceStatics, &bluetoothLEDeviceStatics));
+	CHK(GetVt<zIBluetoothLEDeviceStatics>(bluetoothLEDeviceStatics)->FromBluetoothAddressAsync(bluetoothLEDeviceStatics.Get(), bluetoothAddress, &asyncOperation));
+	CHK(GetVt<zIAsyncOperation>(asyncOperation)->Put_Completed(asyncOperation.Get(), cb2.Get()));
+	completed.wait();
+	CHK(GetVt<zIAsyncOperation>(asyncOperation)->GetResults(asyncOperation.Get(), &bluetoothLEDevice));
+	CHK(bluetoothLEDevice->QueryInterface(uuidIBluetoothLEDevice3, &bluetoothLEDevice3));
+
+	*outBluetoothLEDevice = bluetoothLEDevice;
+	*outBluetoothLEDevice3 = bluetoothLEDevice3;
+}
+
+
+void probe(const ScannedDevice& scannedDevice)
+{
+	wrl::ComPtr<IUnknown> bluetoothLEDevice;
+	wrl::ComPtr<IUnknown> bluetoothLEDevice3;
+
+	deviceFromBluetoothAddress(scannedDevice.m_bluetoothAddress, &bluetoothLEDevice, &bluetoothLEDevice3);
 
 	MCompleted cb2mcompleted;
 	wrl::ComPtr<ComHandler_IAsyncOperationCompletedHandler__BluetoothLEDevice__> cb2 = new ComHandler_IAsyncOperationCompletedHandler__BluetoothLEDevice__(
@@ -68,22 +91,8 @@ void stuff()
 		}
 	);
 
-	wrl::ComPtr<IUnknown> fbaa_op;
-	CHK(GetVt<zIBluetoothLEDeviceStatics>(ledevicesta)->FromBluetoothAddressAsync(ledevicesta.Get(), scanner->m_devices.at(0).m_bluetoothAddress, &fbaa_op));
-
-	CHK(GetVt<zIAsyncOperation>(fbaa_op)->Put_Completed(fbaa_op.Get(), cb2.Get()));
-
-	cb2mcompleted.wait();
-
-	wrl::ComPtr<IUnknown> ledev;
-	wrl::ComPtr<IUnknown> ledev3;
-
-	CHK(GetVt<zIAsyncOperation>(fbaa_op)->GetResults(fbaa_op.Get(), &ledev));
-	
-	CHK(ledev->QueryInterface(uuidIBluetoothLEDevice3, &ledev3));
-
 	wrl::ComPtr<IUnknown> ggswcma_op;
-	CHK(GetVt<zIBluetoothLEDevice3>(ledev3)->GetGattServicesWithCacheModeAsync(ledev3.Get(), (int32_t)zBluetoothCacheMode::Uncached, &ggswcma_op));
+	CHK(GetVt<zIBluetoothLEDevice3>(bluetoothLEDevice3)->GetGattServicesWithCacheModeAsync(bluetoothLEDevice3.Get(), (int32_t)zBluetoothCacheMode::Uncached, &ggswcma_op));
 
 	CHK(ComIsA(uuidIAsyncOperation__GattDeviceServicesResult_star__, ggswcma_op));
 
@@ -99,6 +108,18 @@ void stuff()
 	CHK(GetVt<zIGattDeviceServicesResult>(gdsr)->Status(gdsr.Get(), &gdsr_status));
 
 	CHK(gdsr_status == 0 ? S_OK : E_FAIL);
+}
+
+
+void stuff()
+{
+	wrlw::RoInitializeWrapper initialize(RO_INIT_MULTITHREADED);
+
+	auto scanner = make_unique<Scanner>();
+
+	scanner->wait();
+
+	probe(scanner->m_devices.at(0));
 
 	println(cout, "");
 }
